70819fef3d7ee268ee1b17054172dab6153df0a4
MadWolfInTheHub <95617344+MadWolfInTheHub@users.noreply.github.com>
2022-06-13 16:39:06 +0800

    Update dataFilter.js

--- src/utils/dataFilter.js
+++ src/utils/dataFilter.js
@@ -1,7 +1,8 @@
-export const dataFilter = (flightData, flightToSearch) => {
+export const dataFilter = (flightData, flightToSearch, isDeparture) => {
+  const search = flightToSearch.toLowerCase();
+
   return flightData.filter(flight => 
-    `${flight['carrierID.IATA']}${flight.fltNo}` === flightToSearch
-    || flight.airline.en.name === flightToSearch
-    || flight['airportFromID.city_en'] === flightToSearch
-    || flight['airportToID.city_en']=== flightToSearch);
+    `${flight['carrierID.IATA']}${flight.fltNo}`.toLowerCase().includes(search)
+    || flight.airline.en.name.toLowerCase().includes(search)
+    || !isDeparture ? flight['airportFromID.city_en'].toLowerCase().includes(search) : flight['airportToID.city_en'].toLowerCase().includes(search));
 }
