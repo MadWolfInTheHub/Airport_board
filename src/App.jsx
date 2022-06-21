6d1fe5d31e1dfe4248884a055c6eace661d0a502
MadWolfInTheHub <krivenko.serhii@icloud.com>
2022-06-21 06:41:45 -0700

    test

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,12 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Board from "./components/Board";
 import SearchFlight from "./components/SearchFlight";
 import store from "./store";
+const HI = () => {
+  return (<div>HELLO</div>)
+}
+const Bye = () => {
+  return (<div>Bye</div>)
+}
 
 const App = () => {
   return (
@@ -13,6 +19,11 @@ const App = () => {
           <Route path="/" element={<Board />}>
             <Route path="departures" element={<SearchFlight/>} />
             <Route path="arrivals" element={<SearchFlight/>} />
+            <Route path="" element={<SearchFlight/>}/>
+          </Route>
+          <Route path="/hi" element={<HI />}>
+            <Route path="say" element={<HI/>} />
+            <Route path="no" element={<Bye/>} />
             <Route path="" element={<SearchFlight/>}/>
           </Route>
         </Routes>
